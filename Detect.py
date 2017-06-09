8664b99abcf1707ffb9139435d79a6f94129ecb8
siddharth <siddnusiss@gmail.com>
2017-06-09 21:25:48 +0800

    main.py and Detect.py updated

--- Detect.py
+++ Detect.py
@@ -13,8 +13,11 @@ class Detect:
       # Instantiates a client
       self.vision_client = vision.Client()
       # Instantiates a empty Dictionary
-      self.labelDict = dict()
+      self.labelDictForRoute1 = dict()
+      self.labelDictForRoute2 = dict()
       self.routeDict = dict()
+      self.congestionPresentonRoute1 = False
+      self.congestionPresentonRoute2 = False
 
   def captureImage(self,count):
       self.camera.start_preview()
@@ -22,7 +25,7 @@ class Detect:
       print('count is {0}' .format(count))
       self.camera.capture('image_' + str(count) +'.jpg')
 
-  def processImage(self,filePath):
+  def processImage(self,filePath,routeNo):
       # The name of the image file to annotate
       file_name = os.path.join(
        os.path.dirname(__file__), filePath)
@@ -33,33 +36,42 @@ class Detect:
         # Performs label detection on the image file
         labels = image.detect_labels()
         print('Labels:')
+      if routeNo==1:
+        for label in labels:
+          self.labelDictForRoute1[label.description] = label.score
+        print('Labels for Route 1:')
+        print(self.labelDictForRoute1)
+      if routeNo==2:
+        for label in labels:
+          self.labelDictForRoute2[label.description] = label.score
+        print('Labels for Route 2:')
+        print(self.labelDictForRoute1)  
       
-      for label in labels:
-          self.labelDict[label.description] = label.score
-      print(self.labelDict)
       
-  def createJSON(self,routeNo,configFile):
+  def createJSON(self,configFile):
     with open(configFile) as data_file:
       self.routeDict = json.load(data_file)
-      print self.routeDict
-    for key in self.labelDict.keys():
-      if ('congestion' in key):
-        if(self.labelDict[key]>0.70):
-          
-          if(routeNo == "Route1"):
-            self.routeDict[routeNo] = 1
-            self.routeDict["Route2"] = 0
-          elif(routeNo == "Route2"):
-            self.routeDict[routeNo] = 1
-            self.routeDict["Route1"] = 0
-          print('Congestion on {0}' .format(routeNo))
-          break
-        else:
-          self.routeDict[routeNo] = 0
-          print('No Congestion')
       
-    
-    with open('config.json', 'w') as outfile:
+    for key in self.labelDictForRoute1.keys():
+      if('congestion' in key):
+        self.congestionPresentonRoute1 = True
+        break
+    for key in self.labelDictForRoute2.keys():
+      if('congestion' in key):
+        self.congestionPresentonRoute2 = True
+        break    
+
+    if self.congestionPresentonRoute1 == True:
+      self.routeDict["Route1"] = 1
+    else:
+      self.routeDict["Route1"] = 0
+
+    if self.congestionPresentonRoute2 == True:
+      self.routeDict["Route2"] = 1
+    else:
+      self.routeDict["Route2"] = 0 
+      
+    with open(configFile, 'w') as outfile:
       json.dump(self.routeDict,outfile)
 
 def main():
@@ -67,11 +79,12 @@ def main():
       count = 1
       while (count<5):
         if len(sys.argv) > 1:
-          filePath = sys.argv[1]
-          routeNo = sys.argv[2]
+          filePath_1 = sys.argv[1]
+          filePath_2 = sys.argv[2]
           configFile = sys.argv[3]
-          detect.processImage(filePath)
-          detect.createJSON(routeNo,configFile)
+          detect.processImage(filePath_1,1)
+          detect.processImage(filePath_2,2)
+          detect.createJSON(configFile)
           break
         else:
           detect.captureImage(count)
--- main.py
+++ main.py
@@ -5,17 +5,33 @@ import datetime
 import json
 
 static_command = "python Detect.py "
-fileLocation = "/home/pi/Desktop/Rahul/TrafficControl/image_{0}.jpg "
+congestedImage = "/home/pi/Desktop/TrafficCongestion/TrafficControl/resources/congested.jpg "
+notCongestedImage = "/home/pi/Desktop/TrafficCongestion/TrafficControl/resources/not_congested.jpg "
 routeNo = "Route{0} "
-configFile = "//home/pi/Desktop/Rahul/TrafficControl/config.json"
+configFile = "/home/pi/Desktop/TrafficCongestion/TrafficControl/config/config.json"
 client = Client(client_id="my_id_pub", userdata="user2")
 client.connect("172.23.135.89")
-for i in range(1,3,1):
-    arg1= fileLocation.format(i)
-    arg2= routeNo.format(i)
+for i in range(1,5,1):
+    if i==1:
+        arg1= notCongestedImage
+        arg2= notCongestedImage
+        print("No congestion on any route")
+    if i==2:
+        arg1= notCongestedImage
+        arg2= congestedImage
+        print("congestion on Route 2")
+    if i==3:
+        arg1= congestedImage
+        arg2= notCongestedImage
+        print("congestion on Route 1")
+    if i==4:
+        arg1= congestedImage
+        arg2= congestedImage
+        print("congestion on both Route ")
+        
     os.system(static_command + arg1 + arg2 + configFile)
-    print("Detect.py called for Route {0}" .format(i))
-    with open('config.json') as data_file:
+    
+    with open(configFile) as data_file:
         data = json.load(data_file)
         #print data[arg2]
     topic = "temp"
@@ -23,7 +39,7 @@ for i in range(1,3,1):
 
     print message
     client.publish(topic, message)  
-    time.sleep(10)
+    time.sleep(3)
 
 
 
